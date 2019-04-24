4f00a9e14ba93b87070a6c91c60562a091578ba2
xiaosongfu <xiaosong.fu@outlook.com>
2019-04-24 11:58:03 +0800

    update proto file, add http api option, generate .swagger.json & .pg.gw.go file

--- third_party/protoc-gen.sh
+++ third_party/protoc-gen.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+# gen go grpc file
 protoc --proto_path=api/proto/v1 --proto_path=third_party --go_out=plugins=grpc:pkg/api/v1 todo_service.proto
+
+# gen grpc-gateway file
+protoc --proto_path=api/proto/v1 --proto_path=third_party --grpc-gateway_out=logtostderr=true:pkg/api/v1 todo_service.proto
+
+# gen swagger file
+protoc --proto_path=api/proto/v1 --proto_path=third_party --swagger_out=logtostderr=true:api/swagger/v1 todo_service.proto
